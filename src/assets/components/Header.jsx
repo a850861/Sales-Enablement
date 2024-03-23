9b9095301de1d58248524e35b9e84420cfcfc060
ayan-87 <ayan.cottonian87@gmail.com>
2024-03-23 13:02:01 +0530

    sidebar

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -4,17 +4,26 @@ import logo from '../images/Yara_logo.svg.png'
  
 function Header(){
     return (
-        <header>
-            <ul>
-                <li><img class = 'logo'src={logo} alt="logo" /></li>
-                <li><p> sales Enablement</p></li>
-            </ul>
-            <ul>
-                <li> <a href="#">about </a> </li>
-                <li><a href="">bell</a></li>
-                <li><a href="">account</a></li>
-            </ul>
-        </header>
+        <div className="container">
+            <div class ='header'>
+                <ul>
+                    <li><img class = 'logo'src={logo} alt="logo" /></li>
+                    <li><p> sales Enabler</p></li>
+                </ul>
+                <ul>
+                    <li> <a href="#">about </a> </li>
+                    <li><a href="">bell</a></li>
+                    <li><a href="">account</a></li>
+                </ul>
+            </div>
+            <div className="sidenav">
+                <ul>
+                    <li><a href= "#"> Inventory</a></li>
+                    <li><a href= "#"> Sales History</a></li>
+                </ul>
+            </div>
+        </div>
+        
     )
 }
 export default Header;
